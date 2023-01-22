c0e2cba52f9900fe09f7cb7582e26162c0cdf184
Priyanshu Mishra <mpriyanshu263@gmail.com>
2023-01-22 23:19:44 +0530

    added layerzero constants

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,16 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
+const LZ_ENDPOINTS = require("../constants/layerzeroEndpoints.json");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const NFT = await ethers.getContractFactory("NFT");
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const lzEndpointAddress = LZ_ENDPOINTS[network.name];
+  console.log(`[${network.name}] Endpoint Address: ${lzEndpointAddress}`);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const nft = await NFT.deploy(lzEndpointAddress);
+  await nft.deployed();
 
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log("NFT got deployed at ", nft.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
